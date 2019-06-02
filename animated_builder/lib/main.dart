479bad12b2c24eee17896e8b56a2859a6c5c8d11
Ahmed Nafea Mohamed <32443261+ahmednafea@users.noreply.github.com>
2019-06-02 17:01:25 +0200

    First commit

---
+++ animated_builder/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _SpinnerState createState() => _SpinnerState();
+
+}
+class _SpinnerState extends State<MyApp> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 5),
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      child: Container(width: 40.0, height: 40.0, color: Colors.blue),
+      builder: (BuildContext context, Widget child) {
+        return Transform.rotate(
+          angle: _controller.value * 2.0 * 2.17,
+          child: child,
+        );
+      },
+    );
+  }
+}
