7224d83a10f8a512e1f81813abc6882dc3706d2c
Ahmed Nafea Mohamed <32443261+ahmednafea@users.noreply.github.com>
2019-06-03 11:53:10 +0200

    Fixing Widget

--- animated_container/lib/main.dart
+++ animated_container/lib/main.dart
@@ -2,38 +2,47 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  FlightExampleState createState() {
-    return FlightExampleState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: MyBody(),
+      ),
+    );
   }
 }
-class FlightExampleState extends State<MyApp> {
 
+class MyBody extends StatefulWidget {
+  @override
+  FlightExampleState createState() => FlightExampleState();
+}
+
+class FlightExampleState extends State<MyBody> {
   var _alignment = Alignment.bottomCenter;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AnimatedContainer(
-        padding: EdgeInsets.all(10.0),
-        duration: Duration(seconds: 2),
-        alignment: _alignment,
-        child: Container(
-          height: 50.0,
-          child: Icon(Icons.airplanemode_active, size: 50.0, color: Colors.blueAccent),
+        body: AnimatedContainer(
+          padding: EdgeInsets.all(10.0),
+          duration: Duration(seconds: 2),
+          alignment: _alignment,
+          child: Container(
+            height: 50.0,
+            child: Icon(Icons.airplanemode_active,
+                size: 50.0, color: Colors.blueAccent),
+          ),
         ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton.extended(
-          backgroundColor: Colors.blueAccent,
-          onPressed: (){
-            setState(() {
-              _alignment = Alignment.center;
-            });
-          },
-          icon: Icon(Icons.airplanemode_active),
-          label: Text("Take Flight"))
-    );
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton.extended(
+            backgroundColor: Colors.blueAccent,
+            onPressed: () {
+              setState(() {
+                _alignment = Alignment.center;
+              });
+            },
+            icon: Icon(Icons.airplanemode_active),
+            label: Text("Take Flight")));
   }
 }
