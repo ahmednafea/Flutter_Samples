eac43063beff0a68673211b39457f2d51cddbb94
Ahmed Nafea Mohamed <32443261+ahmednafea@users.noreply.github.com>
2019-06-03 10:40:37 +0200

    Formatting

--- alignment_sample/lib/main.dart
+++ alignment_sample/lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,26 +12,24 @@ class MyApp extends StatelessWidget {
             title: Text('Alert Dialog'),
           ),
           body: MyLayout()),
-
     );
   }
 }
 class MyLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("align"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("align"),
       ),
       body:Container(
-        padding: new EdgeInsets.all(15.0),
+        padding: EdgeInsets.all(15.0),
         child:
         Align(
           alignment: Alignment.bottomLeft,
-          child: Text('Left botom',textScaleFactor: 1.5,),
+          child: Text('Left botom', textScaleFactor: 1.5,),
         ),
       )
     );
   }
 }
-
