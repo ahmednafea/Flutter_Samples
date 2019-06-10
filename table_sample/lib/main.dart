b39498e3a1b2d70ddf6c7bd9bc450e1e2ab41997
Ahmed Nafea Mohamed <32443261+ahmednafea@users.noreply.github.com>
2019-06-10 15:08:20 +0200

    Table Sample

--- table_sample/lib/main.dart
+++ table_sample/lib/main.dart
@@ -1,75 +1,73 @@
 import 'package:flutter/material.dart';
 
-void main() =>
-    runApp(MyApp());
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Table Sample",
-      theme: ThemeData(
-          backgroundColor: Colors.teal
-      ),
+      theme: ThemeData(backgroundColor: Colors.teal),
       home: Scaffold(
           body: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(30.0),
-                child:CircleAvatar(
-                  backgroundColor: Colors.teal,
-                  backgroundImage: NetworkImage("https://media.wired.com/photos/59267d4dcefba457b079a273/master/w_1856,c_limit/Grindr-Logo-TA.jpg"),
-                  maxRadius: 40.0,
-                ),),
-              Divider(),
-              Text("Profile Data",style: TextStyle(fontSize: 22.0,color: Colors.teal)),
-              Container(
-                child: Padding(padding: const EdgeInsets.all(14.0),
-                  child:Table(
-                    border: TableBorder.all(width: 1.0,color: Colors.black),
-                    children:[
-                      TableRow(children: [
-                        TableCell(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: <Widget>[
-                              Text("ID",style: TextStyle(fontSize: 18.0)),
-                              Text("123",style: TextStyle(fontSize: 18.0))
-                            ],
-                          ),
-                        )
-                      ]),
-                      TableRow(children: [
-                        TableCell(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: <Widget>[
-                              Text("Name",style: TextStyle(fontSize: 18.0)),
-                              Text("Ahmed",style: TextStyle(fontSize: 18.0))
-                            ],
-                          ),
-                        )
-                      ]),
-                      TableRow(children: [
-                        TableCell(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: <Widget>[
-                              Text("Salary",style: TextStyle(fontSize: 18.0)),
-                              Text("2500 L.E",style: TextStyle(fontSize: 18.0))
-                            ],
-                          ),
-                        )
-                      ])
-                    ] ,
-                  ) ,
-                ),
-              )
-
-            ],
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: CircleAvatar(
+              backgroundColor: Colors.teal,
+              backgroundImage: NetworkImage(
+                  "https://media.wired.com/photos/59267d4dcefba457b079a273/master/w_1856,c_limit/Grindr-Logo-TA.jpg"),
+              maxRadius: 40.0,
+            ),
+          ),
+          Divider(),
+          Text("Profile Data",
+              style: TextStyle(fontSize: 22.0, color: Colors.teal)),
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Table(
+                border: TableBorder.all(width: 1.0, color: Colors.black),
+                children: [
+                  TableRow(children: [
+                    TableCell(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Text("ID", style: TextStyle(fontSize: 18.0)),
+                          Text("123", style: TextStyle(fontSize: 18.0))
+                        ],
+                      ),
+                    )
+                  ]),
+                  TableRow(children: [
+                    TableCell(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Text("Name", style: TextStyle(fontSize: 18.0)),
+                          Text("Ahmed", style: TextStyle(fontSize: 18.0))
+                        ],
+                      ),
+                    )
+                  ]),
+                  TableRow(children: [
+                    TableCell(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Text("Salary", style: TextStyle(fontSize: 18.0)),
+                          Text("2500 L.E", style: TextStyle(fontSize: 18.0))
+                        ],
+                      ),
+                    )
+                  ])
+                ],
+              ),
+            ),
           )
-      ),
+        ],
+      )),
     );
   }
-
 }
